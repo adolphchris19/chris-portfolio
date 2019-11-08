1bd1d2263c7f2d3ad3e64e952a09278205122171
edmund1645 <edmund.timfon@gmail.com>
2019-11-08 14:47:32 +0100

    add contact page

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Chris Adolphus',
   templates: {
     Portfolio: '/portfolio/:title',
     Tag: '/tag/:id'
---
+++ src/pages/Contact.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout>
+    <h2 class="bottom-line">Looking to get in touch?</h2>
+    <p>
+      If you would like to inquire about a new project, speaking engagements, chat about product design or just say a friendly hello, please feel free to reach via
+      <a
+        class="email-link"
+        href="mailto:adolphchris19@gmail.com"
+      >email</a>.
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Contact Me'
+  }
+};
+</script>
+
+<style lang="scss"scoped>
+h2 {
+  margin-bottom: 2rem;
+}
+.bottom-line {
+  position: relative;
+}
+.bottom-line::after {
+  content: '';
+  height: 3px;
+  width: 80px;
+  background: var(--accent-color);
+  position: absolute;
+  bottom: -10px;
+  left: 0;
+}
+.email-link {
+  text-decoration: underline;
+}
+</style>
